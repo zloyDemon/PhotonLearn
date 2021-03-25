9f771aaa6231f301f23f4c83a31b4d406d56a66c
Marat Yakushev <yakushev1396@gmail.com>
2021-03-25 23:00:27 +0400

    3.Spawn player event.

---
+++ Assets/Scripts/General/PlayerSetupController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSetupController : GlobalEventListener
+{
+    [SerializeField] private GameObject setupPanel;
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        if (!BoltNetwork.IsServer)
+        {
+            setupPanel.SetActive(true);
+        }
+    }
+
+    private void Awake()
+    {
+        var button = setupPanel.GetComponentInChildren<Button>();
+        button.onClick.AddListener(SpawnPlayer);
+    }
+
+    public override void OnEvent(SpawnPlayerEvent evnt)
+    {
+        BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Player, Vector3.up, Quaternion.identity);
+        entity.AssignControl(evnt.RaisedBy);
+    }
+
+    public void SpawnPlayer()
+    {
+        SpawnPlayerEvent evnt = SpawnPlayerEvent.Create(GlobalTargets.OnlyServer); // TODO GlobalTargets need read
+        evnt.Send();
+    }
+}
